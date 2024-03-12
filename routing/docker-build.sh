e235fa02efb55a6936460304c33e2f9f817680dd
josecolman <josecolman@gmail.com>
2024-03-12 09:42:31 -0300

    Ajustes finales de estructura de archivo

--- routing/docker-build.sh
+++ routing/docker-build.sh
@@ -3,14 +3,12 @@ echo 'Building package...'
 ./mvnw clean package -DskipTests -Pproduction
 
 echo 'Local database setup...'
-mkdir mongodb_data
+#mkdir -p /opt/src/deployments/routing-service/mongodb-data
+mkdir -p mongodb_data
 
 echo 'Building image...'
-#docker build --platform linux/amd64 -t 172.16.12.86:443/acciones-comerciales-service:latest .
 docker build -t routing:1.0.0 .
 
-#docker-compose down
-#docker-compose pull
 docker-compose up -d
 
 #spring.data.mongodb.uri=mongodb://appuser:qwerty**@localhost:27017/security
@@ -21,16 +19,3 @@ docker-compose up -d
 #  pwd: 'qwerty**',
 #  roles: [{ role: 'readWrite', db: 'security' }]
 #})
-
-
-#echo 'Pushing image...'
-#docker push 172.16.12.86:443/acciones-comerciales-service:latest
-
-# echo 'Connnecting to test server...'
-# Publicar acciones comerciales en test
-# ssh jose.colman@palermo.com.py@172.16.12.83 'cd /opt/src/deployments/acciones-comerciales'
-#ssh jose.colman@palermo.com.py@172.16.12.83 < docker-update.txt
-
-# cd /opt/src/deployments/acciones-comerciales || exit
-# sudo docker-compose pull
-# sudo docker-compose up -d
